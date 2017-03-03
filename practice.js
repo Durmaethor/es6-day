a42455d114d37bb9f997b26b8be5e230f2166b4a
Ryan Kramer <rkramer613@gmail.com>
2017-03-02 20:05:12 -0700

    destructuring

--- practice.js
+++ practice.js
@@ -88,7 +88,7 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
 
-
+const [notVar, alsoNotVar, ...everythingElse] = vocabAgain;
 
 ////////////
 /* ARRAYS */
